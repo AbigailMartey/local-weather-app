7bee9cdb0fdb94b0a99a204f62cf404516ec4660
AbigailMartey <abigailmartey.n@gmail.com>
2022-11-16 09:02:29 +0100

    added features

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ButtonsComponent } from './buttons/buttons.component';
+import { CurrentWeatherComponent } from './current-weather/current-weather.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    ButtonsComponent
+    CurrentWeatherComponent,
+ 
   ],
   imports: [
     BrowserModule,
